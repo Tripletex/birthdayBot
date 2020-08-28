df04ebdb051fb9f92a303f14809c9f2e4bdfbd25
hn <harry@tripletex.no>
2020-08-28 12:52:04 +0200

    had to change some values camel case is not allowed in config

--- functions/src/tripletexapi/employee.ts
+++ functions/src/tripletexapi/employee.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 import * as functions from 'firebase-functions';
 import { TripletexEmployee } from './types'
 
-const TRIPLETEX_API_URL = 'https://tripletex.no/v2'
+const TRIPLETEX_API_URL = functions.config().tripletex.url
 const employeeEndpoint = '/employee?includeContacts=false&from=0&count=1000'
 
 export async function fetchEmployeeData(): Promise<TripletexEmployee[]> {
@@ -16,7 +16,7 @@ export async function fetchEmployeeData(): Promise<TripletexEmployee[]> {
 }
 
 const requestOptions: AxiosRequestConfig = {
-  method: 'GET', 
+  method: 'GET',
   headers: {
     'content-type': 'application/json',
   }
@@ -33,7 +33,7 @@ const tripletexWrapper = async (endpoint: string, config: AxiosRequestConfig | u
         'authorization': `Basic ${base64Session}`
       }
     }
-    
+
     const response = await makeTripletexRequest(endpoint, requestConfig)
     const tokenDeleted = await deleteTripletexSession(token, requestConfig)
     if (tokenDeleted) {
@@ -51,14 +51,14 @@ tripletexWrapper('/employee', requestOptions)
 
 async function createTripletexSession(): Promise<string> {
   // DO NOT COMMIT THIS
-  const CONSUMER_TOKEN = functions.config().consumerToken
-  const EMPLOYEE_TOKEN = functions.config().employeeToken
+  const CONSUMER_TOKEN = functions.config().tripletex.consumertoken;
+  const EMPLOYEE_TOKEN = functions.config().tripletex.employeetoken;
   const today = new Date()
   const tomorrow = new Date(today)
   tomorrow.setDate(tomorrow.getDate() + 1)
   const expirationDate = `${tomorrow.getFullYear()}-${tomorrow.getMonth() < 9 ? `0${tomorrow.getMonth() + 1}` : tomorrow.getMonth() + 1}-${tomorrow.getDate() < 9 ? `0${tomorrow.getDate()}` : tomorrow.getDate()}`
   const response = await makeTripletexRequest(`/token/session/:create?consumerToken=${CONSUMER_TOKEN}&employeeToken=${EMPLOYEE_TOKEN}&expirationDate=${expirationDate}`, {
-    'method': 'PUT'   
+    'method': 'PUT'
   })
 
   if (response?.data.value && response.data.value.token) {
