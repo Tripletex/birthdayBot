fd5306754ec6704f4eaac82eb97ff0ace840eb78
Harry <harry@tripletex.no>
2022-11-18 08:54:40 +0100

    Upgraded packages and changed to yarn
    Signed-off-by: Harry <harry@tripletex.no>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,6 +15,15 @@ export const testFunction = functions.region('europe-west3').https.onRequest((re
 	});
 });
 
+export const demo = functions.region('europe-west3').https.onRequest((req: https.Request, res: express.Response) =>{
+	fetchTripletexBirthdayEmployees().then((response:Array<TripletexEmployee>) =>{
+		sendBirthdaySlack(response);
+		res.status(200).send({ message: "Message send ok." });
+	}).catch(() => {
+		res.status(500).send({ message: "Error" });
+	});
+});
+
 export const scheduledMorningFunctions = functions.region('europe-west3').pubsub.schedule('0 7 * * *').timeZone('Europe/Oslo').onRun(() => {
 	return fetchTripletexBirthdayEmployees().then((response:Array<TripletexEmployee>) =>{
 		return sendBirthdaySlack(response);
